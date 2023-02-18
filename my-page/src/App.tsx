300fe6f40b2c5474047d3688824ed2c04bebfae5
MeushDashaV <meyshuha@gmail.com>
2023-02-18 12:26:23 +0200

    added props

--- my-page/src/App.tsx
+++ my-page/src/App.tsx
@@ -1,4 +1,14 @@
 import "./App.css";
+import {FC} from "react";
+
+
+type H1Props = {userName: string};
+const AppH1: FC <H1Props> = (props) => (
+  <h1>First name {props.userName}</h1>
+);
+
+
+
 
 const Firstname = () => (
   <div className="first_name">Firstname: Dasha</div>
@@ -20,5 +30,7 @@ const App = () => (
     <Contact/>
   </div>
 );
+
+
 
 export default App;
